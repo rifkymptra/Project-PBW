cf55d32df66e233b836e2c14073fdb26d45b759c
rifkymptra <rifky.mputra8@gmail.com>
2023-06-06 18:08:40 +0700

    add search

--- js/Navbar.js
+++ js/Navbar.js
@@ -1,15 +1,32 @@
-// Toggle class active
+// Toggle class active untuk hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 // Ketika hamburger menu di klik
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
     navbarNav.classList.toggle('active');
+    e.preventDefault();
 };
 
+// Toggle class active untuk search form
+const searchForm = document.querySelector('.search-form');
+const searchButton = document.querySelector('#search-button');
+const searchBox = document.querySelector('#search-box');
+searchButton.onclick = (e) => {
+    searchForm.classList.toggle('active');
+    searchBox.focus();
+    e.preventDefault();
+};
+
+
 // Klik diluar sidebar untuk menghilangkan navbar
-const hamburger = document.querySelector('#hamburger-menu');
+const hm = document.querySelector('#hamburger-menu');
+const sb = document.querySelector('#search-button');
 
 document.addEventListener('click', function(e) {
-    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)) {
         navbarNav.classList.remove('active');
     }
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)) {
+        searchForm.classList.remove('active');
+    }
 })
+
